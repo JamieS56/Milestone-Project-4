e8504aaf45a134c9ba46c859eb93f2cf6c86f04f
JamieS56 <james.sebire@btinternet.com>
2021-10-05 13:05:24 +0000

    set up .env file to hide django secret key that I changed.

--- messiankles/settings.py
+++ messiankles/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c3mvy(83h5=uy+%(#p2ocxun*#pouaqwn=5khr(ybb=m5_!6#a'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
