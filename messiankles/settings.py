26d5482b0ea82770e31658df8e5248f1b47426aa
JamieS56 <james.sebire@btinternet.com>
2021-10-14 16:57:26 +0000

    updated the debug setting and secret key setting so they look in os.environ

--- messiankles/settings.py
+++ messiankles/settings.py
@@ -14,9 +14,6 @@ from pathlib import Path
 
 import os
 import dj_database_url
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['messi-ankles.herokuapp.com', 'localhost']
 
