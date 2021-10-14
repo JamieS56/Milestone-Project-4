4b214ff06366e36b26678e36912ae91cf3685eaa
JamieS56 <james.sebire@btinternet.com>
2021-10-14 18:03:21 +0000

    Added settings to set up aws static files.

--- messiankles/settings.py
+++ messiankles/settings.py
@@ -50,7 +50,11 @@ INSTALLED_APPS = [
 
     # my apps
     'home',
-    'players'
+    'players',
+
+    # other
+    'storages',
+
 ]
 
 MIDDLEWARE = [
@@ -144,6 +148,18 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+if 'USE_AWS' in os.environ:
+    AWS_STORAGE_BUCKET_NAME = 'messi-ankles'
+    AWS_S3_REGION_NAME = 'eu-west-2'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+
+    STATIC_URL = f'{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
