a823c4ccb3b937c8890bcbe1626963b6b2276fdc
JamieS56 <james.sebire@btinternet.com>
2021-10-14 18:07:07 +0000

    Corrected capitalization of static files location.

--- custom_storages.py
+++ custom_storages.py
@@ -3,4 +3,4 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
-    location = settings.StaticFiles_Location
+    location = settings.STATICFILES_LOCATION
