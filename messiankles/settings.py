7dcdcc85fd173b74aaf557b241e14e2c569d3219
JamieS56 <james.sebire@btinternet.com>
2021-10-18 23:28:15 +0000

    Fixing possible typo in the heroku url

--- messiankles/settings.py
+++ messiankles/settings.py
@@ -23,13 +23,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '87k)+7h53v%op+ft(@gs8m_pafy&84r-i6#3u3zqhkwijsbxx@')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
 DEBUG = True
 
-ALLOWED_HOSTS = ['messi-ankles.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['messi-ankles.herokuapp.com', 'localhost']
 
 
 
