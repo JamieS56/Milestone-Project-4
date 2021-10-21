338c1b9cda7427ecb1cb2f82b5e1b38b216b5415
JamieS56 <james.sebire@btinternet.com>
2021-10-21 12:01:55 +0000

    Added allauth templates to my templates folder.

--- messiankles/settings.py
+++ messiankles/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '@p)_eyhr=_(+-q&&ht@mp#!$t5@eoap1b2o3kl*mfzt($rrrjs')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     # my apps
     'home',
     'players',
+    'profiles',
 
     # other
     'storages',
