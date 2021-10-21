1d5b644b0e82a0e9a993f37784c958e82347f50b
JamieS56 <james.sebire@btinternet.com>
2021-10-21 11:15:33 +0000

    Added in a carousel for the home page that only shows 2 players instead of three for smaller screens.

--- home/static/home/js/carousel.js
+++ home/static/home/js/carousel.js
@@ -3,3 +3,9 @@ var carousel = new bootstrap.Carousel(myCarousel, {
   interval: false,
   wrap: true
 })
+
+var myMobileCarousel = document.querySelector('#mobile-player-carousel')
+var mobileCarousel = new bootstrap.Carousel(myMobileCarousel, {
+  interval: false,
+  wrap: true
+})
