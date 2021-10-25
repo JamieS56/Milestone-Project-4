f147c8e99690cf23e9504b60e66ade41c1770881
JamieS56 <james.sebire@btinternet.com>
2021-10-25 12:11:48 +0000

    I have installed crispy forms, and got the login and logout function working properly which also changes whats shown in the nave bar i.e. login and logout buttons.

--- messiankles/settings.py
+++ messiankles/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
     # other
     'storages',
+    'crispy_forms',
 
 ]
 
@@ -69,6 +70,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'messiankles.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -86,6 +89,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media'
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
