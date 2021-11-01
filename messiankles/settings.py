bcb827e98725cde8c65f5e8f8dc176e976bf2623
JamieS56 <james.sebire@btinternet.com>
2021-11-01 17:59:23 +0000

    Reloaded teams fixture as it wasn't loading propperly on heroku so hopefully this fixes it.

--- messiankles/settings.py
+++ messiankles/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '@p)_eyhr=_(+-q&&ht@mp#!$t5@eoap1b2o3kl*mfzt($rrrjs')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
