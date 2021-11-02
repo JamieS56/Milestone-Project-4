6960aa114ae78464f98508e27c23f83109c34be5
JamieS56 <james.sebire@btinternet.com>
2021-11-02 17:21:41 +0000

    Have rolled back to before fixtures app was created and database was corrupted somehow, so just remade teams app.

--- messiankles/settings.py
+++ messiankles/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
-import dj_database_url
-import psycopg2
+
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -23,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'sfgadsbsdvsgjhwr#$%^&fddsfb')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
