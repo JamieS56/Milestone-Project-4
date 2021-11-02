78d9e1e1be15d9c561e4e848d16e579da594715a
JamieS56 <james.sebire@btinternet.com>
2021-11-02 12:32:30 +0000

    importing env file meant push failed because heroku couldn't collect static so commented it out

--- messiankles/settings.py
+++ messiankles/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 import dj_database_url
-import env
+# import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,7 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', env.SECRET_KEY)
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
+# env.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
