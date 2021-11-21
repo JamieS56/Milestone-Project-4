261cb19f7f3037b022840ed78b04732a888ffcaa
JamieS56 <james.sebire@btinternet.com>
2021-11-21 20:59:41 +0000

    home page and nav bar are fully styled across all screen sizes.

--- home/views.py
+++ home/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from helpers import customFunctions
 from players.models import Player
+from teams.models import Fixture
+from datetime import date
 
 
 def index(request):
     ''' A view to return the index page '''
 
     players = customFunctions.add_no_image(Player.objects.all())
+    fixture = Fixture.objects.filter(date__gt=date.today()).order_by('date', 'time').first()
 
     context = {
-        'players': players
+        'players': players,
+        'fixture': fixture
     }
 
     return render(request, 'home/index.html', context)
